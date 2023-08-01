93a65c0494bf1cc27e06fe3beb783a57992a0011
Robin Friedli <robinfriedli@icloud.com>
2023-08-02 00:01:42 +0200

    VideoJS: dispose player on detach

--- src/components/VideoJS.tsx
+++ src/components/VideoJS.tsx
@@ -3,37 +3,43 @@ import videojs, { VideoJsPlayer } from 'video.js';
 import 'video.js/dist/video-js.css';
 
 export const VideoJS = (props: any) => {
-  const videoRef = React.useRef(null);
-  const playerRef = React.useRef<VideoJsPlayer | null>(null);
-  const {options, onReady} = props;
+    const videoRef = React.useRef(null);
+    const playerRef = React.useRef<VideoJsPlayer | null>(null);
+    const { options, onReady } = props;
 
-  React.useEffect(() => {
+    React.useEffect(() => {
 
-    // Make sure Video.js player is only initialized once
-    if (!playerRef.current) {
-      const videoElement = videoRef.current;
+        // Make sure Video.js player is only initialized once
+        if (!playerRef.current) {
+            const videoElement = videoRef.current;
 
-      if (!videoElement) return;
+            if (!videoElement) return;
 
-      const player = playerRef.current = videojs(videoElement, options, () => {
-        videojs.log('player is ready');
-        onReady && onReady(player);
-      });
+            const player = playerRef.current = videojs(videoElement, options, () => {
+                videojs.log('player is ready');
+                onReady && onReady(player);
+            });
 
-    // You could update an existing player in the `else` block here
-    // on prop change, for example:
-    } else {
-      const player = playerRef.current;
+            // You could update an existing player in the `else` block here
+            // on prop change, for example:
+        } else {
+            const player = playerRef.current;
 
-      player.src(options.sources);
-    }
-  }, [options, videoRef]);
+            player.src(options.sources);
+        }
 
-  return (
-    <div data-vjs-player>
-        <video ref={videoRef} className='video-js vjs-big-play-centered' />
-    </div>
-  );
+        return () => {
+            if (playerRef.current) {
+                playerRef.current.dispose();
+            }
+        }
+    }, [options, videoRef]);
+
+    return (
+        <div data-vjs-player>
+            <video ref={videoRef} className='video-js vjs-big-play-centered' />
+        </div>
+    );
 }
 
 export default VideoJS;
